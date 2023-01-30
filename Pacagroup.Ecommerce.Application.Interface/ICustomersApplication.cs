f4986dfcbef3f2c36e41cf037a018948130db083
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2023-01-30 20:55:43 -0300

    Construcción de la Capa de Aplicación

---
+++ Pacagroup.Ecommerce.Application.Interface/ICustomersApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pacagroup.Ecommerce.Application.DTO;
+using Pacagroup.Ecommerce.Transversal.Common;
+using System.Threading.Tasks;
+namespace Pacagroup.Ecommerce.Application.Interface
+{
+    public interface ICustomersApplication
+    {
+        #region Metodos Sincronos
+        Response<bool> Insert(CustomersDto customersDto);
+        Response<bool> Update(CustomersDto customersDto);
+        Response<bool> Delete(string customerId);
+
+        Response<CustomersDto> Get(string customerId);
+        Response<IEnumerable<CustomersDto>> GetAll();
+        #endregion
+
+        #region Metodos Asincronos
+        Task<Response<bool>> InsertAsync(CustomersDto customersDto);
+        Task<Response<bool>> UpdateAsync(CustomersDto customersDto);
+        Task<bool> DeleteAsync(string customerId);
+        Task<Response<CustomersDto>> GetAsync(string customerId);
+        Task<Response<IEnumerable<CustomersDto>>> GetAllAsync();
+        #endregion
+    }
+}
---
+++ Pacagroup.Ecommerce.Transversal.Common/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pacagroup.Ecommerce.Transversal.Common
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public bool IsSucces { get; set; }
+        public string Message { get; set; }
+    }
+}
